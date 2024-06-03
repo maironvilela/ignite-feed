906eea1d3fdc0976858f1226c1c48415b0d548e7
Mairon Vilela <maironvilela@gmail.com>
2024-06-03 19:56:50 -0300

    feat: create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+
+import styles from './styles.module.css'
+  export function Header(){
+    return(
+         <header className={styles.header}>
+            <img src='./logo.svg'/>
+             <h1>Ignite Feed</h1>
+
+         </header>
+    )
+} 
