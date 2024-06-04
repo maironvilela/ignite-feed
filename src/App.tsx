dd8d726fd1e2492ba9a19303f978538f06609e69
Mairon Vilela <maironvilela@gmail.com>
2024-06-04 18:17:53 -0300

    refactor: add props profile componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { Header } from "@components/Header";
-import { Profile } from "@components/profile";
+import { Header } from '@components/Header';
+import { Profile } from '@components/profile';
+import { faker } from '@faker-js/faker';
 
 function App() {
+  const avatarUrl = faker.image.avatar();
+  const name = faker.name.fullName();
+  const profession = faker.commerce.department();
   return (
     <>
       <Header />;
-      <Profile />
+      <Profile avatarUrl={avatarUrl} name={name} profession={profession} />
     </>
   );
 }
--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,15 +1,21 @@
 import styles from './styles.module.css';
 import { faker } from '@faker-js/faker';
 import { PencilSimpleLine } from '@phosphor-icons/react';
-export function Profile() {
+
+type ProfileProps = {
+  name: string;
+  profession: string;
+  avatarUrl: string;
+};
+export function Profile({ name, profession, avatarUrl }: ProfileProps) {
   return (
     <div className={styles.container}>
       <header className={styles.header}></header>
-      <img className={styles.avatar} src={faker.image.avatar()} />
+      <img className={styles.avatar} src={avatarUrl} />
 
       <section className={styles.profile_information}>
-        <strong>Leslie Alexander</strong>
-        <span>UI Designer</span>
+        <strong>{name}</strong>
+        <span>UI {profession}</span>
       </section>
 
       <button className={styles.btn_profile_edit}>
